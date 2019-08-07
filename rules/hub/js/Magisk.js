c7ee0b0102df1c4e75e7bdc474de6eb0cc8017a2
UpgradeAll rule <sdfnkfs@126.com>
2019-08-07 09:38:09 +0800

    Update Magisk.js

--- rules/hub/js/Magisk.js
+++ rules/hub/js/Magisk.js
@@ -1,4 +1,11 @@
+var userAgent =
+  "Mozilla/5.0 (X11; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0";
+
+var url =
+  "https://raw.githubusercontent.com/topjohnwu/magisk_files/master/stable.json";
+
 function getDefaultName() {
+  var returnJson = JSUtils.selNByJsoupXpath(userAgent, url, String xpath)
 }
 
 function getReleaseNum() {
